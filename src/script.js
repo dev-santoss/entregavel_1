eba1fd0aaf3cb0ef619b8872f0ccaee461816f4a
Vitor Santos <vsantosdelimav@gmail.com>
2024-03-27 16:41:22 -0300

    Exercicio de calculadora

--- src/script.js
+++ src/script.js
@@ -1,11 +1,40 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Calculadora(numero1, numero2, Operador) {
+  let resultado
+  let num1 = Number(numero1)
+  let num2 = Number(numero2)
+ 
+  switch (Operador) {
+    case '+':
+      resultado = num1 + num2
+      break;
+    case '-':
+      resultado = num1 - num2
+      break;
+    case '*':
+      resultado = num1 * num2
+      break;
+    case '/':
+      resultado = num1 / num2
+      break;
+    case 'e':
+      contador = 1
+      resultado = num1 * num1
+      while(contador < num2 - 1){
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  
+  }
 
-//ESCREVA SEU CÓDIGO AQUI!!
 
+if ( resultado == undefined || resultado > 1000000)
+resultado = 'ERRO'
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+return resultado
+
+}
+
 
 let visor = document.getElementById("visor");
 
